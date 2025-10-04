13d40a06b84bfb9084133bf4b27c80ba69f9e133
HlavinB-CUNI <27145143@fsv.cuni.cz>
2025-10-04 13:35:22 +0200

    Initial commit of python solution and code files

---
+++ MastersThesisCode/MastersThesisCode/MastersThesisCode.py
@@ -0,0 +1,13 @@
+# Final Project for Bryan Hlavin's Master's Thesis
+# Topic: Do Russian Aggressions Significnatly Impact US Oil Stocks?
+# Made by Bryan Hlavin - Supervisor: Prof. Evzen Kocenda
+
+import pandas as pd
+import numpy as np
+
+# Asking the user what they specifically want to see or calculate
+# Option 1: Collection of all Oil and Gas Stocks which are part of this paper
+
+# Option 2: Calculations for initial analysis (GARCH, etc.)
+
+# Option 3: Graphing of any kind
