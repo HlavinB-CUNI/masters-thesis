9fa60dc2c2771f5e92956d2325ff67dc715181d3
HlavinB-CUNI <27145143@fsv.cuni.cz>
2025-10-09 21:24:55 +0200

    Add in log returns calculations and drop of top row that results in NA. Fix dates from yahoo to be offset by one to compensate for the NA

--- MastersThesisCode/MastersThesisCode/acquire_stock_data_func.py
+++ MastersThesisCode/MastersThesisCode/acquire_stock_data_func.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import os
 import polars as pl
 import yfinance as yahoo
@@ -10,7 +11,6 @@ def acquire_stock_data(path):
 
         # Read all rows (tickers) into a list
         rows = list(csvreader)
-        rows2 = [r[0] for r in rows] 
         
         # Making a polars dataframe of the stocks for show
         rows_polar = pl.DataFrame(rows)
@@ -22,7 +22,8 @@ def acquire_stock_data(path):
 
         # Loop to input each individual stock into the list
         for i in stocks:
-            yahoo_df = yahoo.download(i, start = "2012-07-01", end = "2024-07-01", progress = False, auto_adjust = True)
+            # July 1st, 2012 was a Sunday, so the first return will be on July 2nd, and July 1, 2024 was a Monday, so it will be included
+            yahoo_df = yahoo.download(i, start = "2012-06-29", end = "2024-07-02", progress = False, auto_adjust = True)
             yahoo_df = yahoo_df.reset_index()
             if yahoo_df.empty == False:
                 stock_data[i] = pl.from_pandas(yahoo_df)
@@ -35,17 +36,25 @@ def acquire_stock_data(path):
         # Establishing a new list for specifically adjusted closing returns
         stock_close_data = {}
 
-        # We know that the 12 years of data has 3017 days, so any stock with less than that amount of rows gets disqualified
+        # We know that the 12 years of data has 3017 + 2 days, so any stock with less than that amount of rows gets disqualified
         for j in range(len(stock_data)):
-            if stock_data[j].select(pl.count()).item() == 3017:
+            if stock_data[j].select(pl.count()).item() == 3019:
                 # Making sure only the close price is included
                 stock_close_data[j] = stock_data[j].select(pl.selectors.by_index([0,1]))
 
-        print(stock_close_data)
+                # Adding column for transformed log returns
+                stock_close_data[j] = stock_close_data[j].with_columns([(pl.selectors.by_index([1]).log().diff()).alias("log_close_returns")])
 
-        # Gives the total number of valid stocks for the time period
-        #print(len(stock_close_data))
-        #print(stock_close_data)
+                # Removing the excess NA row at the beginning
+                stock_close_data[j] = stock_close_data[j].select(pl.all().slice(1))
+
+        stock_close_data = list(stock_close_data.values())
+
+        # Adding column for transformed log returns
+
+
+        # Printing output to confirm results
+        print(stock_close_data)
 
 
 
